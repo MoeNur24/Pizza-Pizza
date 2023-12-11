1a586fef75013a96f834fccd435487eae2e810d1
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-12-10 18:43:24 -0800

    Fix branching to PizzaCost function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 function YummyPizza(topping, size) {
     this.topping = topping,
     this.size = size;
-    this.cost = 20;
+    this.cost = 10;
 }
 
 YummyPizza.prototype.pizzaCost = function() {
-    if (this.firstTopping === "SpecialTopping1") {
-        this.cost += 2;
-    }
-    if (this.secondTopping === "SpecialTopping2") {
-        this.cost += 3;
-    }
-    if (this.thirdTopping === "SpecialTopping3") {
-        this.cost += 4;
-    }
-    if (this.fourthTopping === "SpecialTopping4") {
-        this.cost += 5;
+    if ((this.topping + this.size) ==2) {
+        this.cost = 13;
+    } else if ((this.topping + this.size) ==3) {
+       this.cost = 16; 
+    } else if ((this.topping + this.size) ==5) {
+        this.cost = 19;
+    } else {
+        this.cost = 22
     }
 
     return this.cost;
