2f221a38f60ceba38e9dfee9352ba31dd05868e4
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-12-10 19:16:35 -0800

    Update UI Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
+//Business Logic
 function YummyPizza(topping, size) {
     this.topping = topping,
     this.size = size;
     this.cost = 10;
 }
-
 YummyPizza.prototype.pizzaCost = function() {
     if ((this.topping + this.size) ==2) {
         this.cost = 13;
@@ -17,3 +17,12 @@ YummyPizza.prototype.pizzaCost = function() {
 
     return this.cost;
 }
+
+//UI Logic
+document.addEventListener("DOMContentLoaded", function() {
+    const formUno = document.getElementById("formUno");
+    formOne.addEventListener("submit", function(event) {
+      event.preventDefault();
+    
+    });
+  });
